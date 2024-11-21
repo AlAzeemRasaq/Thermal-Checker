f1e5425cc5f0466d2a04fb4adc41902018129114
Al-Azeem Abdul Rasaq <93706390+AlAzeemRasaq@users.noreply.github.com>
2024-11-21 10:38:51 +0000

    Removed "import from Home"

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
 import { Link } from "react-router-dom";
 import "/css/Home.css";
+
 export default class Home extends Component {
     render() {
         return (
