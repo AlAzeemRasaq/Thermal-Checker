cf33addddd7bd81a78a413d1f3d4be2c7b6c62b6
Al-Azeem Abdul Rasaq <93706390+AlAzeemRasaq@users.noreply.github.com>
2024-12-16 20:27:53 +0000

    Create global_constants.js

---
+++ src/components/global_constants.js
@@ -0,0 +1,14 @@
+// This file holds global constants that are visible on the Client-side
+
+// Access level
+export const ACCESS_LEVEL_GUEST = 0;
+export const ACCESS_LEVEL_NORMAL_USER = 1;
+export const ACCESS_LEVEL_ADMIN = 2;
+
+// Server
+export const SERVER_HOST = `http://localhost:4000`;
+
+export const SANDBOX_CLIENT_ID =
+  "AZajxhZoKq7XkGeK4R8O4rJPxDxA6tfgiEhxW2OjQVmYCRdh9NOT0TCi7DleD8Zg4_fFlA7-h-pQ_uZA";
+export const PRODUCTION_CLIENT_ID =
+  "TO_MAKE_A_LIVE_REAL_WORLD_APPLICATION_REPLACE_THIS_TEXT_WITH_YOUR_PRODUCTION_CLIENT_ID";
